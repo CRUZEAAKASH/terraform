9336f6556ae2648bb20b346e643dbcc09466a925
Aakash Shinghal <8235349+CRUZEAAKASH@users.noreply.github.com>
2025-03-16 14:45:48 +0530

    Adding tf file related to multiple block in a single tf file

---
+++ terrafor_practise/multipleBlockInASingleTfFile.tf
@@ -0,0 +1,11 @@
+output "firstBlock" {
+  value = "this is the first block"
+}
+
+output "secondBlock" {
+  value = "This is the second block"
+}
+
+output "thirdBlock" {
+  value = "this is the third block"
+}
