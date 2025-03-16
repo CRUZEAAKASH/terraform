d414bf6ef8926e9000df0a21fd731059b69a98d3
Aakash Shinghal <8235349+CRUZEAAKASH@users.noreply.github.com>
2025-03-16 15:08:21 +0530

    Adding comment to suggest how can we pass variable in a non-interactive mode

---
+++ 6.hello-variable-in-non-interactive-mode/variable.tf
@@ -0,0 +1,7 @@
+// To pass vairable from command line in non-interactive mode to run this in CI/CD etc... 
+// pass the variable name directly from the command line
+// for ex... in this case... terraform plan -var "username=Aakash"
+
+variable "username" {
+
+}
